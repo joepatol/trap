427ea3865cd82e7a0ac2ccbbed1ea86a8c629546
joepatol <j.a.h.p.atol@dnb.nl>
2025-11-19 14:44:56 +0100

    add files

---
+++ aras_py/aras/aras.py
@@ -0,0 +1,35 @@
+import signal
+import asyncio
+
+from aras.aras_types import ASGIApplication, LogLevel
+from aras_core.aras_core import generate_cancel_token, serve_python
+
+
+def serve(
+    application: ASGIApplication,
+    host: str = "127.0.0.1",
+    port: int = 8080,
+    log_level: LogLevel = "INFO",
+    keep_alive: bool = True,
+    max_concurrency: int | None = None,
+    max_size_kb: int = 1_000_000,
+) -> None:
+    loop = asyncio.new_event_loop()
+    token = generate_cancel_token()
+
+    loop.add_signal_handler(signal.SIGINT, token.stop)
+    loop.add_signal_handler(signal.SIGTERM, token.stop)
+    
+    loop.run_until_complete(
+        serve_python(
+            application,
+            token=token,
+            event_loop=loop,
+            addr=[int(i) for i in host.split(".")],
+            port=port,
+            keep_alive=keep_alive,
+            log_leveL=log_level,
+            max_concurrency=max_concurrency,
+            max_size_kb=max_size_kb,
+        )
+    )
---
+++ aras_py/aras/aras_core/aras_core.pyi
@@ -0,0 +1,26 @@
+from typing import Awaitable
+
+from asyncio import BaseEventLoop
+from ..aras_types import ASGIApplication, LogLevel, CancelToken
+
+
+__all__ = [
+    "generate_cancel_token",
+    "serve_python",
+]
+
+
+def generate_cancel_token() -> CancelToken: ...
+
+
+def serve_python(
+    application: ASGIApplication,
+    token: CancelToken,
+    event_loop: BaseEventLoop,
+    addr: list[int] = [127, 0, 0, 1],
+    port: int = 8080,
+    keep_alive: bool = True,
+    log_leveL: LogLevel = "INFO",
+    max_concurrency: int | None = None,
+    max_size_kb: int = 1_000_000,
+) -> Awaitable[None]: ...
